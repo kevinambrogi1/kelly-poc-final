348b8cdcfe13a20d8dcb350fbd29d0265c2a73d5
Kevin Ambrogi <kevinambrogi@hotmail.com>
2017-06-12 19:08:53 +0200

    fixed environment prod URL and added force https config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "https://kelly-poc-final.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
+
+  # Force https
+  config.force_ssl = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
