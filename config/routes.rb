930253f60bedd6b722f70ee75ab0c206075acccf
kevinambrogi1 <kevinambrogi@hotmail.com>
2017-08-21 13:33:06 +0200

    fixed field length

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # get 'subscribers/index'
+  get 'subscribers/index'
   resources :subscribers
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #
-  # get 'subscribers/index'
 
 end
